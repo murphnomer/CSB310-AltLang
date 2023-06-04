fa71aa5c6b12f8a9a367e05c6764b1be42eccd1e
murphnomer <elmiko@gmail.com>
2023-06-03 22:29:35 -0700

    Parser completed

--- Cell/Cell.swift
+++ Cell/Cell.swift
@@ -9,8 +9,8 @@ import Foundation
 
 class Cell {
     struct Dimension2 {
-        var Width : Float = 0.0
-        var Height : Float = 0.0
+        var Width : Int = 0
+        var Height : Int = 0
     }
     
     struct Dimension3 {
@@ -21,25 +21,112 @@ class Cell {
     
     struct Display {
         var Tech : String = ""
-        var Size : Int = 0
-        var Resolution : Dimension2
+        var Size : Float?
+        var Resolution : Dimension2 = Dimension2()
     }
-    
+
     var Manufacturer : String = ""
     var Model : String = ""
-    var DateAnnounced : Int = 0
-    var DateReleased : String = ""
-    var LaunchStatus : String = ""
-    var Dimensions : Dimension3
-    var Weight : Float = 0.0
-    var SIMType : String = ""
-    var DisplayType : String = ""
-    var DisplaySize : Dimension2
-    var DisplayResolution : String = ""
+    var YearAnnounced : Int?
+    var YearReleased : Int?
+    var LaunchStatus : String?
+    var Dimensions : Dimension3?
+    var Weight : Float?
+    var SIMType : String?
+    var Display : Display = Display()
     var Features : String = ""
-    var OSPlatform : String = ""
- 
-    init() {
-        
+    var OSPlatform : String?
+    
+    func parseManufacturer(value : String) -> String {
+        self.Manufacturer = value
+        return self.Manufacturer
     }
+    
+    func parseModel(value : String) -> String {
+        self.Model = value
+        return self.Model
+    }
+    
+    func parseYearAnnounced(value : String) -> Int? {
+        if let match = value.firstMatch(of: /^(\d{4})/ ) {
+            YearAnnounced = Int(match.1)
+        }
+        return self.YearAnnounced
+
+    }
+
+    func parseYearReleased(value : String) -> Int? {
+        if let match = value.firstMatch(of: /(\d{4})/ ) {
+            YearReleased = Int(match.1)
+        }
+        return self.YearReleased
+    }
+
+    func parseLaunchStatus(value : String) -> String? {
+        if let match = value.firstMatch(of: /(\d{4}|Discontinued|Cancell?ed)/ ) {
+            LaunchStatus = String(match.1)
+        }
+        return self.LaunchStatus
+    }
+    
+    func parseDimensions(value : String) -> Dimension3? {
+        if let match = value.firstMatch(of: /([\d.]+) x ([\d.]+) x ([\d.]+) mm/ ) {
+            Dimensions = Dimension3()
+            Dimensions!.Height = Float(match.1)!
+            Dimensions!.Width = Float(match.2)!
+            Dimensions!.Thickness = Float(match.3)!
+        }
+        return self.Dimensions
+    }
+    
+    func parseWeight(value : String) -> Float? {
+        if let match = value.firstMatch(of: /([\d.]+) g/ ) {
+            self.Weight = Float(match.1)!
+        }
+        return self.Weight
+    }
+    
+    func parseSIMType(value : String) -> String? {
+        if value != "Yes" && value != "No" {
+            self.SIMType = value
+        }
+        return self.SIMType
+    }
+    
+    func parseDisplayType(value : String) -> String {
+        self.Display.Tech = value
+        return self.Display.Tech
+    }
+    
+    func parseDisplaySize(value : String) -> Float? {
+        if let match = value.firstMatch(of: /([\d.]+) cm/ ) {
+            self.Display.Size = Float(match.1)!
+        }
+        return self.Display.Size
+    }
+    
+    func parseDisplayResolution(value : String) -> String {
+        if let match = value.firstMatch(of: /([\d]+) x ([\d]+) pixels/ ) {
+            self.Display.Resolution.Width = Int(match.1)!
+            self.Display.Resolution.Height = Int(match.2)!
+        }
+        var reso = "\(self.Display.Resolution.Height) x \(self.Display.Resolution.Width)"
+        return reso
+    }
+    
+    func parseFeatures(value : String) -> String {
+        self.Features = value
+        return self.Features
+    }
+    
+    func parseOSPlatform(value : String) -> String? {
+        if let match = value.firstMatch(of: /([^,]+),?/ ) {
+            self.OSPlatform = String(match.1)
+        }
+        return self.OSPlatform
+    }
+    
+
+
+
 }
--- Cell/main.swift
+++ Cell/main.swift
@@ -6,6 +6,35 @@
 //
 
 import Foundation
+import SwiftCSV
 
-print("Hello, World2!")
+let cellFileURL : String = "https://raw.githubusercontent.com/murphnomer/CSB310-AltLang/main/cells.csv"
+var cells : [Cell] = []
+
+do {
+    let csv: CSV = try CSV<Named>(url: URL(string:cellFileURL)!)
+    print("CSV has \(csv.rows.count) rows")
+    for r in csv.rows {
+        var cell : Cell = Cell()
+        cell.parseManufacturer(value: r["oem"] ?? "")
+        cell.parseModel(value: r["model"] ?? "")
+        cell.parseYearAnnounced(value: r["launch_announced"] ?? "")
+        cell.parseYearReleased(value: r["launch_status"] ?? "")
+        cell.parseLaunchStatus(value: r["launch_status"] ?? "")
+        cell.parseDimensions(value: r["body_dimensions"] ?? "")
+        cell.parseWeight(value: r["body_weight"] ?? "")
+        cell.parseSIMType(value: r["body_sim"] ?? "")
+        cell.parseDisplayType(value: r["display_type"] ?? "")
+        cell.parseDisplaySize(value: r["display_size"] ?? "")
+        cell.parseDisplayResolution(value: r["display_resolution"] ?? "")
+        cell.parseFeatures(value: r["features_sensors"] ?? "")
+        cell.parseOSPlatform(value: r["platform_os"] ?? "")
+        cells.append(cell)
+        
+    }
+    print("Got \(cells.count) cell objects")
+
+} catch {
+    print(error)
+}
 
